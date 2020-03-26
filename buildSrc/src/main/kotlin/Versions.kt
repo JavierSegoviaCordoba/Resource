f143e4b25e2e51cc0a25f46b0b22668c5d8a0b1f
Javier Segovia Córdoba <javiersegoviacordoba@gmail.com>
2020-03-26 02:16:37 +0100

    Update: Kotlin 1.3.71

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,7 +1,7 @@
 object Versions {
     const val versions = "0.25.0"
     const val detekt = "1.0.1"
-    const val kotlin = "1.3.70"
+    const val kotlin = "1.3.71"
     const val jvmTarget = "1.8"
     const val coroutines = "1.3.4"
     const val retrofit = "2.8.1"
