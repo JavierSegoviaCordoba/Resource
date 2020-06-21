ceecf178d6f7b53c3f2ab7fe6aee15dd51ee9b0c
Javier Segovia Córdoba <javiersegoviacordoba@gmail.com>
2020-06-21 20:18:14 +0200

    Add success and error maps

--- resource/src/commonMain/kotlin/com/javiersc/resources/resource/extensions/Resource.kt
+++ resource/src/commonMain/kotlin/com/javiersc/resources/resource/extensions/Resource.kt
@@ -18,10 +18,38 @@ import kotlinx.coroutines.flow.flow
 inline fun <reified R, reified R2, reified E, reified E2> Resource<R, E>.map(
     data: (R) -> R2,
     error: (E) -> E2,
-): Resource<R2, E2> {
+): Resource<R2, E2> = when (this) {
+    is Resource.Loading -> this
+    is Resource.Success -> Resource.Success(data(this.data))
+    is Resource.Error -> Resource.Error(error(this.error))
+}
+
+/**
+ * Map any Resource to another Resource.
+ * @param R is the original resource type.
+ * @param E is the original error type.
+ * @param R2 the new resource type.
+ * @param data to be mapped to [R2].
+ */
+inline fun <reified R, reified R2, reified E> Resource<R, E>.mapSuccess(data: (R) -> R2): Resource<R2, E> {
     return when (this) {
-        is Resource.Loading -> Resource.Loading
+        is Resource.Loading -> this
         is Resource.Success -> Resource.Success(data(this.data))
+        is Resource.Error -> this
+    }
+}
+
+/**
+ * Map any Resource to another Resource.
+ * @param R is the original resource type.
+ * @param E is the original error type.
+ * @param E2 the new error type.
+ * @param error to be mapped to [E2].
+ */
+inline fun <reified R, reified E, reified E2> Resource<R, E>.mapError(error: (E) -> E2): Resource<R, E2> {
+    return when (this) {
+        is Resource.Loading -> this
+        is Resource.Success -> this
         is Resource.Error -> Resource.Error(error(this.error))
     }
 }
--- resource/src/commonTest/kotlin/MapperTest.kt
+++ resource/src/commonTest/kotlin/MapperTest.kt
@@ -4,7 +4,10 @@ import com.javiersc.resources.resource.Resource.Success
 import com.javiersc.resources.resource.extensions.asError
 import com.javiersc.resources.resource.extensions.asSuccess
 import com.javiersc.resources.resource.extensions.map
+import com.javiersc.resources.resource.extensions.mapError
+import com.javiersc.resources.resource.extensions.mapSuccess
 import utils.DataAndCounters.Companion.ERROR_DATA
+import utils.DataAndCounters.Companion.ERROR_DATA_2
 import utils.DataAndCounters.Companion.SUCCESS_DATA
 import utils.DataAndCounters.Companion.SUCCESS_DATA_2
 import kotlin.test.Test
@@ -13,27 +16,31 @@ import kotlin.test.assertTrue
 internal class MapperTest {
 
     @Test
-    fun `check success to success mapper`() {
+    fun `check mapper`() {
         val originalResource: Resource<String, Unit> = Success(SUCCESS_DATA)
-        val finalResource: Resource<String, Unit> =
-            originalResource.map(data = { SUCCESS_DATA_2 }, error = { Unit })
+        val finalResource: Resource<String, Unit> = originalResource.map(data = { SUCCESS_DATA_2 }, error = { Unit })
+        with(finalResource) { assertTrue { this is Success && data == SUCCESS_DATA_2 } }
 
-        with(finalResource) {
-            assertTrue { this is Success && data == SUCCESS_DATA_2 }
-        }
+        val originalResource2: Resource<Unit, String> = Error(ERROR_DATA)
+        val finalResource2: Resource<Unit, String> = originalResource2.map(data = { Unit }, error = { ERROR_DATA_2 })
+        with(finalResource2) { assertTrue { this is Error && error == ERROR_DATA_2 } }
     }
 
     @Test
     fun `check success mapper`() {
-        val res: Resource<String, Unit> = SUCCESS_DATA.asSuccess()
-        with(res) {
-            assertTrue { this is Success && data == SUCCESS_DATA }
-        }
+        val originalResource: Resource<String, Unit> = SUCCESS_DATA.asSuccess()
+        with(originalResource) { assertTrue { this is Success && data == SUCCESS_DATA } }
+
+        val finalResource = originalResource.mapSuccess { SUCCESS_DATA_2 }
+        with(finalResource) { assertTrue { this is Success && data == SUCCESS_DATA_2 } }
     }
 
     @Test
     fun `check error mapper`() {
-        val res: Resource<Unit, String> = ERROR_DATA.asError()
-        with(res) { assertTrue { this is Error && error == ERROR_DATA } }
+        val originalResource: Resource<Unit, String> = ERROR_DATA.asError()
+        with(originalResource) { assertTrue { this is Error && error == ERROR_DATA } }
+
+        val finalResource = originalResource.mapError { ERROR_DATA_2 }
+        with(finalResource) { assertTrue { this is Error && error == ERROR_DATA_2 } }
     }
 }
