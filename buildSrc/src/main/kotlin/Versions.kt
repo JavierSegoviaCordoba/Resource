2609354b839480439d58f39837844b8b11a6382d
Javier Segovia Córdoba <javiersegoviacordoba@gmail.com>
2020-10-26 21:10:09 +0100

    Update dependencies

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -2,11 +2,11 @@ val versions: Versions get() = Versions
 
 object Versions {
     const val dependencyUpdates = "0.33.0"
-    const val detekt = "1.14.1"
-    const val coroutines = "1.3.9"
-    const val kotest = "4.2.5"
+    const val detekt = "1.14.2"
+    const val coroutines = "1.4.0"
+    const val kotest = "4.3.0"
     const val kotlin = "1.4.10"
     const val nexus = "0.22.0"
-    const val serialization = "1.0.0-RC2"
+    const val serialization = "1.0.0"
     const val turbine = "0.2.1"
 }
