dc225aee38b7c899321eaa52e79a353fb56df921
Javier Segovia Córdoba <javiersegoviacordoba@gmail.com>
2020-10-29 01:36:25 +0100

    Upgrade dependencies

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -7,6 +7,6 @@ object Versions {
     const val kotest = "4.3.0"
     const val kotlin = "1.4.10"
     const val nexus = "0.22.0"
-    const val serialization = "1.0.0"
+    const val serialization = "1.0.1"
     const val turbine = "0.2.1"
 }
